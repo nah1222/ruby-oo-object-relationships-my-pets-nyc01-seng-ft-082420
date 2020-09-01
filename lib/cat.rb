8cff946d0cfa6b5215346078d1fdeb780bca3a81
Nicholas Ho <nah1222@yahoo.com>
2020-09-01 12:37:29 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,12 @@
-class Cat
-  # code goes here
+attr_reader :name
+  def initialize(name, owner)
+    @name = name 
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+  @@all= []
+  def self.all
+    @@all
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,57 @@
 class Owner
   # code goes here
+  attr_reader :name, :species
+  @@all= []
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all<<self
+  end
+  def say_species
+    "I am a #{self.species}."
+  end
+  
+  def self.all 
+    @@all
+  end
+  def self.reset_all
+    self.all.clear 
+  end
+  def self.count
+    self.all.count
+  end
+  
+  def cats 
+    Cat.all.filter{|cat| cat.owner == self}
+  end
+
+  def dogs
+    Dog.all.filter{|dog| dog.owner == self}
+  end
+
+  def buy_cat (name)
+    Cat.new(name, self)
+  end
+
+  def buy_dog (name)
+    Dog.new(name, self)
+  end
+  def walk_dogs
+    self.dogs.each{|dog| dog.mood = "happy"}
+  end
+  def feed_cats
+    self.cats.each{|cat| cat.mood = "happy"}
+  end
+  def sell_pets
+    self.dogs.each{|dog| 
+      dog.mood = "nervous"
+      dog.owner = nil}
+    
+    self.cats.each{|cat| 
+      cat.mood = "nervous" 
+      cat.owner = nil}
+  end 
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
 end
